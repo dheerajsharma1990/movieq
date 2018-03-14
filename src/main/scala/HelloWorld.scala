235de9ff32409e2093e64588e6688a93646635f6
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2018-03-14 15:28:28 +0100

    basic learning of scala objects and types

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,7 @@
+import com.movieq.domain.Movie
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    Movie.printAllMovies()
   }
 }
--- src/main/scala/com/movieq/domain/Movie.scala
+++ src/main/scala/com/movieq/domain/Movie.scala
@@ -1,5 +1,31 @@
 package com.movieq.domain
 
-class Movie {
+class Movie(val id: Long, val title: String) {
 
+  def this(id: Long) = {
+    this(id, "")
+  }
+
+  def wordsInTitle(): Int = {
+    title.split(" ").length
+  }
+
+  def hasSameTitleLength(movie: Movie): Boolean = {
+    title.length == movie.title.length
+  }
+
+  override def toString: String = {
+    "Id: " + id + " Title: " + title
+  }
+}
+
+object Movie {
+
+  private val allMovies = List(new Movie(123L, "Kaminey"));
+
+  def printAllMovies(): Unit = {
+    for (movie <- allMovies) {
+      println(movie)
+    }
+  }
 }
